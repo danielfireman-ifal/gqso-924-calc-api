8af95b88f2960b65cfd01625d1d6c47d2ba93514
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-15 19:02:18 +0000

    Implementação mult e tests ok

--- mult.go
+++ mult.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func mult(a, b float64) float64 {
@@ -11,5 +14,17 @@ func mult(a, b float64) float64 {
 func multHandler(
 	w http.ResponseWriter,
 	r *http.Request) {
-
+	path := strings.Split(r.URL.Path, "/")
+	op1, err := strconv.ParseFloat(path[2], 64)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	op2, err := strconv.ParseFloat(path[3], 64)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	result := mult(op1, op2)
+	fmt.Fprintf(w, "%f", result)
 }
