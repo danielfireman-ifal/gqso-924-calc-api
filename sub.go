186aa17d7569fddd505591366dc5e9cb4ad922c1
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-15 18:51:17 +0000

    Finalizando código do subHandler

--- sub.go
+++ sub.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
 func sub(a, b float64) float64 {
 	return a - b
+}
+
+func subHandler(
+	w http.ResponseWriter,
+	r *http.Request) {
+	path := strings.Split(r.URL.Path, "/")
+	op1, err := strconv.ParseFloat(path[2], 64)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	op2, err := strconv.ParseFloat(path[3], 64)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	result := sub(op1, op2)
+	fmt.Fprintf(w, "%f", result)
 }
--- sub_test.go
+++ sub_test.go
@@ -19,10 +19,32 @@ func TestSubHandler_OK(t *testing.T) {
 		"http://test.com/sub/2/1",
 		nil)
 	w := httptest.NewRecorder()
-	somaHandler(w, req)
+	subHandler(w, req)
 
 	resp := w.Result()
 	body, _ := io.ReadAll(resp.Body)
 	result, _ := strconv.ParseFloat(string(body), 64)
 	assert.Equal(t, 1., result)
+}
+
+func TestSubHandler_Error2Param(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/sub/1/4d",
+		nil)
+	w := httptest.NewRecorder()
+	subHandler(w, req)
+	resp := w.Result()
+	assert.Equal(t, 400, resp.StatusCode)
+}
+
+func TestSubHandler_Error1Param(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/sub/4d/1",
+		nil)
+	w := httptest.NewRecorder()
+	subHandler(w, req)
+	resp := w.Result()
+	assert.Equal(t, 400, resp.StatusCode)
 }
