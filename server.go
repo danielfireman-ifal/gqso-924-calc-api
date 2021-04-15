ef2fba96ddec86920710649fcdb790ab4ec67475
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-15 19:03:12 +0000

    Adicionando rota /mult/

--- server.go
+++ server.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	http.HandleFunc("/soma/", somaHandler)
 	http.HandleFunc("/sub/", subHandler)
+	http.HandleFunc("/mult/", multHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
