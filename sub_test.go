3d6b54e58a4ac77ac6add3ec75740c2e7c22ee83
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-15 18:47:01 +0000

    test subhandler falhando

--- sub_test.go
+++ sub_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +11,18 @@ import (
 
 func TestSub_OK(t *testing.T) {
 	assert.Equal(t, 1., sub(2, 1))
+}
+
+func TestSubHandler_OK(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/sub/2/1",
+		nil)
+	w := httptest.NewRecorder()
+	somaHandler(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	result, _ := strconv.ParseFloat(string(body), 64)
+	assert.Equal(t, 1., result)
 }
