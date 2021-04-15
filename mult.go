842675c349018ae5df1a5e87e856100c49214c53
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-15 19:01:40 +0000

    tests para multHandler

--- mult.go
+++ mult.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"net/http"
+)
+
 func mult(a, b float64) float64 {
 	return a * b
+}
+
+func multHandler(
+	w http.ResponseWriter,
+	r *http.Request) {
+
 }
--- mult_test.go
+++ mult_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +11,40 @@ import (
 
 func TestMult(t *testing.T) {
 	assert.Equal(t, 4., mult(2, 2))
+}
+
+func TestMultHandler_OK(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/mult/2/1",
+		nil)
+	w := httptest.NewRecorder()
+	multHandler(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	result, _ := strconv.ParseFloat(string(body), 64)
+	assert.Equal(t, 2., result)
+}
+
+func TestMultHandler_Error2Param(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/mult/1/4d",
+		nil)
+	w := httptest.NewRecorder()
+	multHandler(w, req)
+	resp := w.Result()
+	assert.Equal(t, 400, resp.StatusCode)
+}
+
+func TestMultHandler_Error1Param(t *testing.T) {
+	req := httptest.NewRequest(
+		"GET",
+		"http://test.com/mult/4d/1",
+		nil)
+	w := httptest.NewRecorder()
+	multHandler(w, req)
+	resp := w.Result()
+	assert.Equal(t, 400, resp.StatusCode)
 }
