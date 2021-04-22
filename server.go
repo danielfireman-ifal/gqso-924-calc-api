a8c01cb1acdaa3b1bec0dccb46d05e3b5c0aa023
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-04-22 19:02:18 +0000

    Usando variável de ambiente PORT

--- server.go
+++ server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/soma/", somaHandler)
 	http.HandleFunc("/sub/", subHandler)
 	http.HandleFunc("/mult/", multHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
 }
